60162db6fc0948ddf2d69a675dccccd442d058b0
tynakpinska <m.k.kepinska@gmail.com>
2022-11-22 11:09:46 +0100

    add support of the keyboard and calculate operations with three digits

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 let display = document.querySelector(".operation");
 let result = document.querySelector(".result");
 
-const operate = (displayContent, regOperator) => {
-  let numbers = [];
-  if (regOperator.test(displayContent[0])) {
-    numbers[0] = displayContent[0] + displayContent[1];
-    let shortenedDisplayContent = displayContent.slice(2, displayContent.length);
-    numbers.splice(1, 0, ...shortenedDisplayContent.split(regOperator));
-  } else {
-    numbers = displayContent.split(regOperator);
-  }
-  numbers = numbers.filter(n => n !== "");
-  let operator = displayContent.match(regOperator)
-    ? displayContent.match(regOperator)[0]
-    : undefined;
-  let num1 = numbers[0] ? Number(numbers[0]) : undefined;
-  let num2 = numbers[1] ? Number(numbers[1]) : undefined;
-
+const calculate = (num1, num2, operator) => {
+  console.log("num1: ", num1, "num2: ", num2, "operator: ", operator);
   if (operator === "/" && num2 === 0) {
     result.textContent = "error";
     return;
@@ -25,21 +11,64 @@ const operate = (displayContent, regOperator) => {
     result.textContent = num1;
     return;
   }
+  if (num1 && num2) {
+    switch (operator) {
+      case "+":
+        return (num1 + num2).toString().length > 10
+          ? (num1 + num2).toPrecision(10)
+          : num1 + num2;
+      case "-":
+        return (num1 - num2).toString().length > 10
+          ? (num1 - num2).toPrecision(10)
+          : num1 - num2;
+      case "*":
+        return (num1 * num2).toString().length > 10
+          ? (num1 * num2).toPrecision(10)
+          : num1 * num2;
 
-  switch (operator) {
-    case "+":
-      result.textContent = (num1 + num2).toString().length > 10 ? (num1 + num2).toPrecision(10) : num1 + num2;
-      break;
-    case "-":
-      result.textContent = (num1 - num2).toString().length > 10 ? (num1 - num2).toPrecision(10) : num1 - num2;
-      break;
-    case "*":
-      result.textContent = (num1 * num2).toString().length > 10 ? (num1 * num2).toPrecision(10) : num1 * num2;
-      break;
-    case "/":
-      result.textContent = (num1 / num2).toString().length > 10 ? (num1 / num2).toPrecision(10) : num1 / num2;
-      break;
+      case "/":
+        return (num1 / num2).toString().length > 10
+          ? (num1 / num2).toPrecision(10)
+          : num1 / num2;
+    }
   }
+};
+
+const parseDisplayContent = (displayContent, regOperator) => {
+  let numbers = [];
+  if (regOperator.test(displayContent[0])) {
+    numbers[0] = displayContent[0] + displayContent[1];
+    let shortenedDisplayContent = displayContent.slice(
+      2,
+      displayContent.length
+    );
+    numbers.splice(1, 0, ...shortenedDisplayContent.split(regOperator));
+  } else {
+    numbers = displayContent.split(regOperator);
+  }
+  numbers = numbers.filter(n => n !== "");
+  console.log(numbers);
+
+  let num1 = numbers[0] ? Number(numbers[0]) : undefined;
+  let num2 = numbers[1] ? Number(numbers[1]) : undefined;
+
+  let operators = displayContent.match(regOperator);
+
+  let operator = operators
+    ? num1 < 0
+      ? operators[1]
+      : operators[0]
+    : undefined;
+
+  if (numbers.length > 2) {
+    num1 = calculate(num1, num2, operator);
+    numbers.shift();
+    numbers[0] = num1;
+    num2 = numbers[1] ? Number(numbers[1]) : undefined;
+    operator = displayContent.match(regOperator)[1];
+  }
+
+  result.textContent = calculate(num1, num2, operator);
 };
 
 const removeLastChar = () => {
@@ -58,9 +87,9 @@ const replaceOperator = (reg, clickedOperator) => {
 };
 
 const updateDisplay = e => {
-  let regOperator = new RegExp(/[\+\-\*\/]/);
+  let regOperator = new RegExp(/[\+\-\*\/]/g);
   let regOperatorEnd = new RegExp(/[\+\-\*\/]$/);
-  let clickedChar = e.target.textContent;
+  let clickedChar = e.key ? e.key : e.target.textContent;
 
   if (clickedChar === "CLEAR") {
     display.textContent = "";
@@ -83,18 +112,18 @@ const updateDisplay = e => {
 
   if (clickedChar === "🠄") {
     removeLastChar();
-    operate(display.textContent, regOperator);
+    parseDisplayContent(display.textContent, regOperator);
     return;
   }
 
   if (clickedChar === "=") {
-    operate(display.textContent, regOperator);
+    parseDisplayContent(display.textContent, regOperator);
     display.textContent = result.textContent;
     return;
   }
 
   addClickedChar(clickedChar);
-  operate(display.textContent, regOperator);
+  parseDisplayContent(display.textContent, regOperator);
 };
 
 const buttons = document.querySelectorAll("button");
@@ -102,3 +131,5 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(button => {
   button.addEventListener("mouseup", updateDisplay);
 });
+
+window.addEventListener("keydown", updateDisplay);
